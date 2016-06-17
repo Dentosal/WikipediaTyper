3c15f7bd4bf8b234ce979a3a802703c61be9ac2d
Dentosal <hannes.karppila@gmail.com>
2016-06-17 23:55:46 +0300

    Update

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var cutEdge = (typeof(Storage) !== "undefined") && 'serviceWorker' in navigator;
 var pageQueue = ["philosophy"];
 var displayQueue = [];
+var visitedPages = [];
 
 function loadNext() {
     if (pageQueue.length === 0) {
@@ -8,8 +9,12 @@ function loadNext() {
     }
     var title = pageQueue.splice(Math.random()*pageQueue.length, 1)[0].replace(" ", "_");
 
-    $.get("https://en.wikipedia.org/api/rest_v1/page/summary/title?redirect=true").done(function (data) {
+    $.get("https://en.wikipedia.org/api/rest_v1/page/summary/"+title+"?redirect=true").done(function (data) {
         console.log("!", data);
+    }, "JSON");
+
+    $.get("https://en.wikipedia.org/api/rest_v1/page/related/"+title+"?redirect=true").done(function (data) {
+        console.log("?", _.pluck(data.items, "title"));
     }, "JSON");
 
     pageQueue = _.unique(pageQueue);
