481a5d2e3d1a09b7bfad3a655288edc50d21dfe5
Dentosal <hannes.karppila@gmail.com>
2016-06-17 23:36:36 +0300

    Update

--- main.js
+++ main.js
@@ -2,5 +2,17 @@ var cutEdge = (typeof(Storage) !== "undefined") && 'serviceWorker' in navigator;
 
 
 $.ready(function () {
-
+    $.get("https://en.wikipedia.org/api/rest_v1/page/html/philosophy?redirect=true").success(function (data) {
+        console.log("!", data);
+    });
+    // $.ajax({
+    //     headers: {
+    //         Accept : "text/plain; charset=utf-8",
+    //         "Content-Type": "text/plain; charset=utf-8"
+    //     },
+    //     data: "data",
+    //     success : function(response) {
+    //     ...
+    //     }
+    // });
 });
