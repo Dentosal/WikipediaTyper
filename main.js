8ab431a244555c02f3d8c8d3bc1c9faf64db3349
Dentosal <hannes.karppila@gmail.com>
2016-06-18 00:03:00 +0300

    Update

--- main.js
+++ main.js
@@ -3,24 +3,33 @@ var pageQueue = ["philosophy"];
 var displayQueue = [];
 var visitedPages = [];
 
+function normalizeTitle(title) {
+    return title.toLowerCase().replace(" ", "_");
+}
+
 function loadNext() {
     if (pageQueue.length === 0) {
         console.log("EMPTY?!");
     }
-    var title = pageQueue.splice(Math.random()*pageQueue.length, 1)[0].replace(" ", "_");
+    var title = normalizeTitle(pageQueue.splice(Math.random()*pageQueue.length, 1)[0]);
 
     $.get("https://en.wikipedia.org/api/rest_v1/page/summary/"+title+"?redirect=true").done(function (data) {
-        console.log("!", data);
+        $("#content").append("<p>"+data.extract+"</p>");
     }, "JSON");
 
-    $.get("https://en.wikipedia.org/api/rest_v1/page/related/"+title+"?redirect=true").done(function (data) {
-        console.log("?", _.pluck(data.items, "title"));
-    }, "JSON");
-
-    pageQueue = _.unique(pageQueue);
+    if (pageQueue.length < 1000) {
+        $.get("https://en.wikipedia.org/api/rest_v1/page/related/"+title+"?redirect=true").done(function (data) {
+            _.each(_.pluck(data.items, "title"), function(title) {
+                var t = normalizeTitle(title);
+                if (!(_.contains(visitedPages, t) || _.contains(displayQueue, t))) {
+                    pageQueue.push(t);
+                }
+            })
+        }, "JSON");
+    }
 }
 
 
 $(document).ready(function() {
-
+    setInterval(loadNext, 2500);
 });
