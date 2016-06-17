18f02b56408f7e2bbc2c42feb5a706b050f0fdf8
Dentosal <hannes.karppila@gmail.com>
2016-06-18 00:23:38 +0300

    Update

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function loadNext() {
 
     if (pageQueue.length < 1000) {
         $.get("https://en.wikipedia.org/api/rest_v1/page/related/"+title+"?redirect=true").done(function(data) {
-            console.log(data.items);
+            console.log(_.pluck(data.items, "title"));
             _.each(_.pluck(data.items, "title"), function(title) {
                 var t = normalizeTitle(title);
                 if (!(_.contains(visitedPages, t) || _.contains(displayQueue, t))) {
